9b329c889e8650f2a94170722cbd81bfebd343bb
Owen Vey <owen.vey@gmail.com>
2023-03-03 19:15:17 -0600

    add tailwind and Inter

--- app.vue
+++ app.vue
@@ -1,5 +1,4 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div class="text-xl text-blue-700 font-semibold">Test</div>
+  <div class="text-xl text-blue-700 font-bold">Test</div>
 </template>
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
