d4bdb8a03b2fb1866680aff5fbed3c2741829dd8
Owen Vey <owen.vey@gmail.com>
2023-03-09 19:52:15 -0600

    optimize the evolution-chain api call to fetch all species in parallel

--- server/api/evolution-chain/[id].ts
+++ server/api/evolution-chain/[id].ts
@@ -1,20 +1,44 @@
-import { ChainLinkWithSpecies, type ChainLink, type EvolutionChain, type EvolutionChainWithSpecies } from '~/types';
+import {
+  type ChainLinkWithSpecies,
+  type PokemonSpecies,
+  type ChainLink,
+  type EvolutionChain,
+  type EvolutionChainWithSpecies,
+} from '~/types';
 import { getLastSegmentOfUrl } from '@/utils/getLastSegmentOfUrl';
 
 export default defineEventHandler(async (event): Promise<EvolutionChainWithSpecies> => {
   const id = event.context.params?.id;
+
   const evolutionChain = await $fetch<EvolutionChain>(`https://pokeapi.co/api/v2/evolution-chain/${id}`);
-  return { ...evolutionChain, chain: await recursivelyfetchSpecies(evolutionChain.chain) };
+  const ids = getSpeciesIds(evolutionChain.chain);
+  const speciesResults = await Promise.all(ids.map((id) => $fetch(`/api/pokemon-species/${id}`)));
+  const chainLinkWithSpecies = replaceSpeciesWithData(evolutionChain.chain, speciesResults);
+
+  return { ...evolutionChain, chain: chainLinkWithSpecies };
 });
 
-async function recursivelyfetchSpecies(evolution: ChainLink): Promise<ChainLinkWithSpecies> {
+// returns an array of ids of all the pokemon-species found in the evolution-chain request tree
+// so that they can all be fetched in parallel instead of fetching as the tree is traversed
+function getSpeciesIds(evolution: ChainLink, ids: string[] = []) {
   const speciesId = getLastSegmentOfUrl(evolution.species.url);
-  const species = await $fetch(`/api/pokemon-species/${speciesId}`);
-
-  evolution.species = species;
+  speciesId && ids.push(speciesId);
 
   if (evolution.evolves_to?.length > 0) {
-    await Promise.all(evolution.evolves_to.map(recursivelyfetchSpecies));
+    evolution.evolves_to.map((e) => getSpeciesIds(e, ids));
+  }
+
+  return ids;
+}
+
+// replaces the NamedAPIResource "species" the fetched pokemon-species data from the function above
+function replaceSpeciesWithData(evolution: ChainLink, species: PokemonSpecies[]): ChainLinkWithSpecies {
+  const matchedSpecies = species.find((s) => s.name === evolution.species.name);
+
+  evolution.species = matchedSpecies;
+
+  if (evolution.evolves_to?.length > 0) {
+    evolution.evolves_to.map((e) => replaceSpeciesWithData(e, species));
   }
 
   return evolution;
